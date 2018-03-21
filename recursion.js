f26b6af1636be7f94a94cae53c72fedfa8fff521
Anthony <anthony@anthonys-mbp.lan>
2018-03-21 19:50:19 -0400

    Got the recursion down

--- recursion.js
+++ recursion.js
@@ -7,12 +7,18 @@ while (n <= 10) {
   n++;
 }
 
-// write a recursive - function called countToTen that mimics the while loop above.
+// // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+const countToTen = (n) => {
+  while (n <= 10) {
+    console.log('While Loop', n);
+    return countToTen(++n);
+  }
+};
 
-// when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+// // when you code is ready, un-comment the next line and run the file
+console.log(countToTen());
 /* ================ Next Problem ================= */
 
 // Problem 2:
@@ -28,6 +34,18 @@ const factorial = n => {
 console.log(factorial(5));
 
 // write the above function in a recursive way.
+const recursiveFactorial = (n) => {
+  let result = 1;
+  let i = 2;
+  const loop = () => {
+    if (i > n) return result;
+    result *= i;
+    return loop(++i);
+  }
+  return loop(n);
+};
 
-// when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+// // when your code is ready, un-comment the next line and run the file
+
+console.log(recursiveFactorial(5));
+
