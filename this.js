177d774f3fdf183d37f2c0f1c8d506870774b147
Anthony <anthony@anthonys-mbp.lan>
2018-03-21 22:49:37 -0400

    updated this.js with answers and samples

--- this.js
+++ this.js
@@ -1,10 +1,45 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the for principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Bingings are called when "this" is not explicitly nor implicitly bound.
+     It serves as a default binding and will be whatever the binding of the current
+     window is. 
+
+     const noObject = () => {
+        console.log(this);
+     };
+     as no object is implicitly nor explicity declared, "this" returns the window obj.
+     
+* 2. Implicit Bindings bind the "this" keyword to whatever object is directly to the
+     left of "this" in the row in which it is invoked.
+
+     const myObj = () => {
+         a: "hi",
+         b: "bye",
+         greet: someFunc (parameter) {
+            console.log(`${this.b} loser`)
+         }
+     };
+
+* 3. New Bindings are functions that produce an object with key:value pairs, and the 
+     "this" keyword is bound to the parameter that is passed into the function. 
+     
+     const builder = (x) {
+        this.firstKey = "1";
+        this.secondKey = "2";
+     };
+
+* 4. Explicit Bindings are more specific than implicit, and by using .call() or 
+     .apply(), we can override the implicit binding rules and specify the object
+     to associate "This" with.
+
+     Obj 1 {
+         key1: value1
+     } 
+     Obj 2 {
+         key1: value2
+     }
+     obj1.key1.call(obj2);
 *
 * write out a code example of each explanation above
 */
